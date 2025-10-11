734dc69c7c988182ba4ea59a2617b0193f7bd367
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-11 15:30:58 +0300

    zad5 and zad 6

---
+++ UP_PRAKTIKUM/praktikum/zad5.cpp
@@ -0,0 +1,16 @@
+//5. Напишете програма, която взима две числа от стандартния вход и извежда 
+// * true * ако първото е по - малко от второто и * false * в противен случай.
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	double num1 = 0, num2 = 0;
+//	cout << "Please enter the first number: ";
+//	cin >> num1;
+//	cout << "Please enter the second number: ";
+//	cin >> num2;
+//
+//	bool is1Less = num1 < num2;
+//	cout << num1 << " is less than " << num2 << ": " << boolalpha << is1Less << "\n";
+//}	
---
+++ UP_PRAKTIKUM/praktikum/zad6.cpp
@@ -0,0 +1,17 @@
+//6. Да се напише програма, която чете от конзолата 2 числа и отпечатва остатъка и частното при делението им.<br / >
+
+	//#include <iostream>
+	//using namespace std;
+	//
+	//int main() {
+	//	int dividend, divisor;
+	//	cout << "Whats the dividend? \n";
+	//	cin >> dividend;
+	//	cout << "Whats the divisor? \n";
+	//	cin >> divisor;
+	//
+	//	int quotient = dividend / divisor;
+	//	int remainder = dividend % divisor;
+	//
+	//	cout << "The quotient of: " << dividend << " and " <<	divisor << " is " << quotient << " with remainder of: " << remainder;
+	//}
