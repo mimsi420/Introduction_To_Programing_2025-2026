60377a4ed1094d72c00fe0659a12e65697330e58
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-14 17:44:54 +0300

    zad4 sem1

---
+++ SEMINAR/zad4.cpp
@@ -0,0 +1,13 @@
+//** Задача 4:**Въвежа се цяло число.Да се отпечата 1, ако числото е валидна оценка от училище, и 0, ако не е валидна.
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int isGrade;
+//	cout << "Enter an integer: \n";
+//	cin >> isGrade;
+//	bool validGrade = (isGrade >= 2 && isGrade <= 6);
+//	cout << "The number is a valid school grade (1 = true, 0 = false): " << validGrade;
+//}
