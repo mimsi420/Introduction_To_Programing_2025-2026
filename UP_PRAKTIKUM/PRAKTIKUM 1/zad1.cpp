d37579b6bddb6ae78c039a5d3b8639797503a869
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-14 19:45:59 +0300

    zad1 prakt1 update

--- UP_PRAKTIKUM/PRAKTIKUM 1/zad1.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 1/zad1.cpp
@@ -3,9 +3,7 @@
 //using namespace std;
 //
 //int main() {
-//    cout << "Oh what a happy day! \n";
-//    cout << "Oh yes,\n";
-//    cout << "what a happy day! \n";
+//    cout << "Oh what \na happy day! \nOh yes,\nwhat a happy day! \n";
 //}
 
 //1. Напишете програма на C++, която извежда следния текст на екрана :
