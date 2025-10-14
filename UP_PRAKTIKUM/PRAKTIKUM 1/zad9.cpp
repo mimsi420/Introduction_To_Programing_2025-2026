2ba25dd8c03a90099941804df1d1ff9b28eb326a
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-14 22:18:08 +0300

    zad9 prakt1

---
+++ UP_PRAKTIKUM/PRAKTIKUM 1/zad9.cpp
@@ -0,0 +1,22 @@
+﻿//9. Да се въведат от конзолата коефициенти за квадратното уравнение ax2 + bx + c.Да се намерят корените на уравнението.<br / >
+//**Mоже да се използва <cmath>(math.h) sqrt * *<br / >
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a = 0, b = 0, c = 0;
+
+	cout << "(ax2 + bx + c) SOLVER \n Give a coefficient for a: \n";
+	cin >> a;
+	cout << "\n Give a coefficient for b: \n";
+	cin >> b;
+	cout << "\n Give a coefficient for c: \n";
+	cin >> c;
+
+	double D = (b * b) - (4 * a * c);
+	double x1 = (-b + sqrt(D)) / (2 * a);
+	double x2 = (-b - sqrt(D)) / (2 * a);
+
+	cout << "The roots of (" << a << "x^2 + " << b << "x + " << c << ") are: \n" << x1 << "\n" << x2 << "\n";
+}
