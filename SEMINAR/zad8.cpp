fe30e104f3c890bce30b6c6aa90339e5ecdba381
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-14 19:23:52 +0300

    zad9 sem1

--- SEMINAR/zad8.cpp
+++ SEMINAR/zad8.cpp
@@ -1,15 +1,15 @@
 ﻿//** Задача 8:**Въвеждат се две числа.Да се отпечата по - голямото от двете.
 
-#include <iostream>
-using namespace std;
-
-int main() 
-{
-	double num1 = 0, num2 = 0;
-
-	cout << "Enter two different numbers: ";
-	cin >> num1 >> num2;
-
-	bool isNum1Bigger = num1 > num2, isNum2Bigger = num2 > num1;
-	cout << "The bigger number is: \n" << isNum1Bigger * num1 + isNum2Bigger * num2;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() 
+//{
+//	double num1 = 0, num2 = 0;
+//
+//	cout << "Enter two different numbers: ";
+//	cin >> num1 >> num2;
+//
+//	bool isNum1Bigger = num1 > num2, isNum2Bigger = num2 > num1;
+//	cout << "The bigger number is: \n" << isNum1Bigger * num1 + isNum2Bigger * num2;
+//}
---
+++ SEMINAR/zad9.cpp
@@ -0,0 +1,19 @@
+	//** Задача 9:**Имате две променливи от тип int да се разменят техните стойности.
+
+	//#include <iostream>
+	//using namespace std;
+
+	//int main() 
+	//{
+	//	int num1, num2;
+	//	cout << "Enter two integers: ";
+	//	cin >> num1 >> num2;
+
+	//	cout << "Before swapping: \n" << "First integer: " << num1 << "\nSecond integer: " << num2 << "\n" << "\n";
+
+	//	num1 = num1 + num2;
+	//	num2 = num1 - num2;
+	//	num1 = num1 - num2;
+
+	//	cout << "After swapping: \n" << "First integer: " << num1 << "\nSecond integer: " << num2;
+	//}
