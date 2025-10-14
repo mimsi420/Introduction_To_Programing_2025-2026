71d5302a7e05e960929b72da0a4b602de50afc16
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-14 18:37:31 +0300

    fixed zad6 sem1

---
+++ SEMINAR/zad6.cpp
@@ -0,0 +1,15 @@
+﻿//** Задача 6:**Напишете програма, която въвежда две числа - a и b и извежда като резултат(a + b) ^ 4 - (a - b) ^ 2
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a = 0, b = 0;
+
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+
+	double result = ((a + b) * (a + b) * (a + b) * (a + b) - (a - b) * (a - b));
+	cout << "Result is: " << result;
+}
