4a82727642ae014c2bb9f7a523fe5743107cb549
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-14 21:45:30 +0300

    zad8 prakt1

---
+++ UP_PRAKTIKUM/PRAKTIKUM 1/zad8.cpp
@@ -0,0 +1,20 @@
+﻿//8. Да се напише програма, която:
+//А) Потребителят въвежда радиус на окръжност;
+//Б) Програмата изчислява дължината на окръжността;
+//В) Програмата изчислява лицето на окръжността при зададения радиус(PI = 3, 14159265).
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	const double PI = 3.14159265;
+	double r = 0;
+
+	cout << "Enter radius: ";
+	cin >> r;
+
+	double circumference = 2 * PI * r, area = PI * r * r;
+
+	cout << "Circumference: " << circumference << "(" << 2 * r << " PI)";
+	cout << "\nArea: " << area << "(" << r * r << " PI)";
+}
