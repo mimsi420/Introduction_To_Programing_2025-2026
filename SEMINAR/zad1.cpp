3ecfacfc227d81ed1fc1f4765763d66d768fa027
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-14 17:38:45 +0300

    zad 2 i zad 3 seminar 1

--- SEMINAR/zad1.cpp
+++ SEMINAR/zad1.cpp
@@ -1,12 +1,12 @@
-//** Задача 1:**Въвежда се цяло число.Отпечатайте 1, ако числото е четно и 0, ако е нечетно.
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int num;
-	cin >> "Enter an integer: \n";
-	cin >> num;
-	cout << (num % 2 == 0);
-}
+////** Задача 1:**Въвежда се цяло число.Отпечатайте 1, ако числото е четно и 0, ако е нечетно.
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int num;
+//	cout << "Enter an integer: \n";
+//	cin >> num;
+//	cout << "Your number is even (1 = true, 0 = false): " << (num % 2 == 0);
+//}
---
+++ SEMINAR/zad2.cpp
@@ -0,0 +1,18 @@
+//** Задача 2:**Въвежда се трицифрено цяло число n.Да се отпечата обърнатото число събрано с единица.
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cout << "Enter a three-digit integer: \n";
+//	cin >> n;
+//
+//	int hundreds = n / 100;
+//	int tens = (n / 10) % 10;
+//	int ones = n % 10;
+//
+//	int reversed = ones * 100 + tens * 10 + hundreds;
+//	cout << "Reversed number plus one: " << reversed + 1;
+//}
