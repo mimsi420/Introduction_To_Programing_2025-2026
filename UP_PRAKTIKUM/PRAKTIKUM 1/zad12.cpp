31e844ed0aaed1eb95dc7c8cca34ab33ba0ac974
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-15 21:03:43 +0300

    11+12 praktikum

---
+++ UP_PRAKTIKUM/PRAKTIKUM 1/zad12.cpp
@@ -0,0 +1,16 @@
+﻿////12. Да се напише програма, която чете от конзолата брой секунди и изчислява колко дни, часове, минути и секунди са.
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int secs;
+//	cout << "Enter seconds: ";
+//	cin >> secs;
+//
+//	int days = secs / 86400;
+//	int hours = (secs % 86400) / 3600;
+//	int mins = (secs % 3600) / 60;
+//	int secsAfter = secs % 60;
+//
+//	cout << secs << "s is " << days << " days, " << hours << " hours, " << mins << " minutes and " << secsAfter << " seconds.\n";
+//}
