791ab76d61a403c2a7ab4f9e73d7e7796cbe439d
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-15 21:35:49 +0300

    14

--- UP_PRAKTIKUM/PRAKTIKUM 1/zad13.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 1/zad13.cpp
@@ -1,26 +1,25 @@
 ﻿//13. Напишете програма, която приема от входа две двойки реални числа, отговарящи на две точки в равнината.
 //Да се отпечата растоянието между точките закръглено до 2 - рия знак след десетичната запетая.
-
-#include <iostream>
-using namespace std;
-
-int main() {
-	double Ax, Ay, Bx, By;
-	cout << "Enter coordinates of A.\nx: ";
-	cin >> Ax;
-	cout << "y: ";
-	cin >> Ay;
-	cout << "\nEnter coordinates of B.\nx: ";
-	cin >> Bx;
-	cout << "y: ";
-	cin >> By;
-	
-	cout << "Your points are: A(" << Ax << ", " << Ay << ") and B(" << Bx << ", " << By << ").\n";
-
-	double dist = sqrt(((Ax - Bx) * (Ax - Bx)) + ((Ay - By) * (Ay - By)));
-
-	dist = round(dist * 100) / 100;
-
-	cout << "The distance between them is: " << dist << "\n";
-
-}
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	double Ax, Ay, Bx, By;
+//	cout << "Enter coordinates of A.\nx: ";
+//	cin >> Ax;
+//	cout << "y: ";
+//	cin >> Ay;
+//	cout << "\nEnter coordinates of B.\nx: ";
+//	cin >> Bx;
+//	cout << "y: ";
+//	cin >> By;
+//	
+//	cout << "Your points are: A(" << Ax << ", " << Ay << ") and B(" << Bx << ", " << By << ").\n";
+//
+//	double dist = sqrt(((Ax - Bx) * (Ax - Bx)) + ((Ay - By) * (Ay - By)));
+//
+//	dist = round(dist * 100) / 100;
+//
+//	cout << "The distance between them is: " << dist << "\n";
+//}
--- UP_PRAKTIKUM/PRAKTIKUM 1/zad14.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 1/zad14.cpp
@@ -1,1 +1,14 @@
-﻿
+﻿//14. Напишете програма която приема номер на кредитна карта(16 цифри). <br / >
+//Програмата да изведе същият този номер, но със скрити първите 12 цифри(XXXXXXXXXXXX1234).
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int long long cardNumber;
+//
+//	cout << "Enter your credit card number (16 digits): ";
+//	cin >> cardNumber;
+//	
+//	cout << "Your credit card number is: XXXXXXXXXXXX" << cardNumber % 10000;
+//}
