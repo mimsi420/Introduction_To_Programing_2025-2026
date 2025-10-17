8f2efc5c88e75f427bb1c21c326b5c8c31c35eec
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-17 09:47:56 +0300

    prakt2/ zad1 i 2

---
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad1.cpp
@@ -0,0 +1,21 @@
+﻿//1. Напишете програма, която проверява дали въведена от потребителя година е високосна.
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int year;
+//
+//	cout << "Enter year: ";
+//	cin >> year;
+//
+//	int remainder4 = year % 4;
+//	int remainder100 = year % 100;
+//	int remainder400 = year % 400;
+//
+//	if ((remainder4 == 0 && remainder100 != 0) || (remainder400 == 0)) {
+//		cout << "This year " << year << " is a leap year.\n";
+//	} 
+//	else {
+//		cout << "This year " << year << " is not a leap year.\n";
+//	}
+//}
---
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad2.cpp
@@ -0,0 +1,30 @@
+﻿//2. Напишете програма, която приема един символ от входа и проверява дали :
+//-този символ е буква от латинската азбука;
+//-ако да, дали тази буква е гласна или съгласна.
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	char symbol;
+//	cout << "Enter a symbol: ";
+//	cin >> symbol;
+//
+//	if ((symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z'))
+//	{
+//		cout << "The symbol " << symbol << " is from the english alphabet.\n";
+//		if (symbol == 'A' || symbol == 'E' || symbol == 'I' || symbol == 'O' || symbol == 'U' ||
+//			symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u')
+//		{
+//			cout << "It is a vowel.\n";
+//		}
+//		else
+//		{
+//			cout << "It is a consonant.\n";
+//		}
+//	}
+//	else
+//	{
+//		cout << "The symbol " << symbol << " is not from the english alphabet.\n";
+//	}
+//}
