963db1b62116c3b88bdb8ee1e912149d28ee5e12
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-21 16:18:25 +0300

    zad3 prakt2

---
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad3.cpp
@@ -0,0 +1,14 @@
+﻿//3. Напишете програма, която приема две** различни** числа и казва кое е по голямо. <br / >
+//**Използвайте тернарен оператор.* *
+
+#include <iostream>;
+using namespace std;
+
+int main() 
+{
+	int firstNum, secondNum;
+
+	cout << "Enter two different numbers: ";
+
+
+}
