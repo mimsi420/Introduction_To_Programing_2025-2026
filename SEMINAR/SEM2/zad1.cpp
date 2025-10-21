882e3fe4f009a5abba375b0ffdcf877b7a292424
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-21 16:36:25 +0300

    zad1 sem2

---
+++ SEMINAR/SEM2/zad1.cpp
@@ -0,0 +1,42 @@
+﻿// Задача 1:Да се напише програма, която въвежда цяло число отпечатава с думи кой ден от седмицата е.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int dayInt;
+
+	cout << "Enter a number (1-7): ";
+	cin >> dayInt;
+
+	if (dayInt > 1 && dayInt < 7 ) 
+	{
+		switch (dayInt) {
+		case 1:
+			cout << "Monday\n";
+			break;
+		case 2:
+			cout << "Tuesday\n"; \
+				break;
+		case 3:
+			cout << "Wednesday\n";
+			break;
+		case 4:
+			cout << "Thursday\n";
+			break;
+		case 5:
+			cout << "Friday\n";
+			break;
+		case 6:
+			cout << "Saturday\n";
+			break;
+		case 7:
+			cout << "Sunday\n";
+			break;
+		}
+	}
+	else
+	{
+		cout << "Invalid input! Please enter a number between 1 and 7.\n";
+	}
+}
