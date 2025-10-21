8b03e5b21fbf9c3caeb6417bfa6241cd8fdfeb21
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-21 17:26:23 +0300

    sem2 zad2

---
+++ SEMINAR/SEM3/zad2.cpp
@@ -0,0 +1,27 @@
+﻿//** Задача 2:**Да се напише програма, която приема цяло положително число, обръща го и го увеличава с 1. < br / >
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num, reversedNum = 0;
+
+	cout << "Enter a positive integer: \n";
+	cin >> num;
+
+	if (num >= 0)
+	{
+		for (num; num != 0; num = num / 10)
+		{
+			reversedNum = reversedNum * 10 + num % 10;
+		}
+
+		reversedNum++;
+		cout << "The reversed number 1 plus 1 is : " << reversedNum << "\n";
+	}
+	else
+	{
+		cout << "The number is not positive or is not an integer.\n";
+	}
+}
