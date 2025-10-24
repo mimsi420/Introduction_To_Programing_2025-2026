18bee787b8f35fc612e382c0810e9acb3629b816
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-24 13:43:42 +0300

    prakt2 zad10

---
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad10.cpp
@@ -0,0 +1,38 @@
+﻿//10. Напишете прост калкулатор, който поддържа операциите + , -, *, / на две реални числа. <br / >
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	double num1, num2;
+//	char op;
+//
+//	cout << "Enter 2 real numbers: \n";
+//	cin >> num1 >> num2;
+//
+//	cout << "Enter operator (+, -, *, /): \n";
+//	cin >> op;
+//
+//	switch (op)
+//	{
+//		case '+':
+//			cout << num1 << " + " << num2 << " = " << num1 + num2 << "\n";
+//			break;
+//		case '-':
+//			cout << num1 << " - " << num2 << " = " << num1 - num2 << "\n";
+//			break;
+//		case '*':
+//			cout << num1 << " * " << num2 << " = " << num1 * num2 << "\n";
+//			break;
+//		case '/':
+//			if (num2 != 0)
+//			{
+//				cout << num1 << " / " << num2 << " = " << num1 / num2 << "\n";
+//			}
+//			else
+//			{
+//				cout << "Error: Division by zero is not allowed.\n";
+//			}
+//	}
+//}
