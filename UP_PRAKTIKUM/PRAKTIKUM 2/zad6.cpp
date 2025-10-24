bbffd984a2dcb3bf5da6cbaa096ab79572ed98b6
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-24 09:27:28 +0300

    prakt2 zad6 7

---
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad6.cpp
@@ -0,0 +1,31 @@
+﻿//6. Напишете програма, която намира корените на квадратно уравнение. <br / >
+//За целта потребителя да въведе коефицентите на уравнението - *a*, * b*, и* c* . <br / >
+//при уравнение с вид * a \ * x ^ 2 + b \ * x + c = 0 * . <br / >
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	double a, b, c;
+//	cout << "ax^2 + bx + c = 0 SOLVER\nEnter coefficients a, b and c: ";
+//	cin >> a >> b >> c;
+//
+//	double D = b * b - 4 * a * c;
+//	if (D > 0)
+//	{
+//		double x1 = ((-b + sqrt(D)) / 2 * a);
+//		double x2 = ((-b - sqrt(D)) / 2 * a);
+//
+//		cout << "Two real roots found. x1 = " << x1 << " and x2 = " << x2 << "\n";
+//	}
+//	else if (D == 0)
+//	{
+//		double x = (-b / (2 * a));
+//		cout << "One real root found. x = " << x << "\n";
+//	}
+//	else
+//	{
+//		cout << "No real roots found.\n";
+//	}
+//}
---
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad7.cpp
@@ -0,0 +1,25 @@
+﻿//7. Да се напише програма, която чете от конзолата символ и, ако е малка буква, извежда съответната голяма, 
+// ако е голяма буква, извежда съответната малка, а ако не е буква, да изведе съобщение за невалиден символ.
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	char ch;
+//	cout << "Enter a letter from the alphabet: ";
+//	cin >> ch;
+//	if (ch >= 'a' && ch <= 'z') 
+//	{
+//		char upperCh = ch - ('a' - 'A'); // all the lower- and uppercase letters have a fixed offset so this works for every char
+//		cout << "Uppercase: " << upperCh << endl;
+//	} 
+//	else if (ch >= 'A' && ch <= 'Z') 
+//	{
+//		char lowerCh = ch + ('a' - 'A');
+//		cout << "Lowercase: " << lowerCh << endl;
+//	} 
+//	else 
+//	{
+//		cout << "Invalid character" << endl;
+//	}
+//}
