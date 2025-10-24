6552ab4b613e1c0700c31a017778d09ea26f8241
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-24 09:39:52 +0300

    prakt2 zad8

---
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad8.cpp
@@ -0,0 +1,48 @@
+﻿//8. Да се напише програма, която чете от конзолата едноцифрено число и извежда същото число, записано с римски цифри. <br / >
+//(I = 1, V = 5,X = 10)
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() 
+//{
+//	int number;
+//	cout << "Enter a single-digit number (0-9): ";
+//	cin >> number;
+//
+//	switch (number)
+//	{
+//		case 0:
+//			cout << "Roman numeral: Doesn't exist but N (nulla) is often used.\n";
+//			break;
+//		case 1:
+//			cout << "Roman numeral: I\n";
+//			break;
+//		case 2:
+//			cout << "Roman numeral: II\n";
+//			break;
+//		case 3:
+//			cout << "Roman numeral: III\n";
+//			break;
+//		case 4:
+//			cout << "Roman numeral: IV\n";
+//			break;
+//		case 5:
+//			cout << "Roman numeral: V\n";
+//			break;
+//		case 6:
+//			cout << "Roman numeral: VI\n";
+//			break;
+//		case 7:
+//			cout << "Roman numeral: VII\n";
+//			break;
+//		case 8:
+//			cout << "Roman numeral: VIII\n";
+//			break;
+//		case 9:
+//			cout << "Roman numeral: IX\n";
+//			break;
+//		default: 
+//			cout << "Invalid input.\n";
+//	}
+//}
