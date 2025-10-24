93cdd5ad709d1b56cd2f654791605b5b0a262644
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-24 08:46:26 +0300

    prakt2 zad3 zad4

--- UP_PRAKTIKUM/PRAKTIKUM 2/zad3.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad3.cpp
@@ -1,14 +1,18 @@
 ﻿//3. Напишете програма, която приема две** различни** числа и казва кое е по голямо. <br / >
 //**Използвайте тернарен оператор.* *
 
-#include <iostream>;
-using namespace std;
-
-int main() 
-{
-	int firstNum, secondNum;
-
-	cout << "Enter two different numbers: ";
-
-
-}
+//#include <iostream>;
+//using namespace std;
+//
+//int main() 
+//{
+//	double firstNum, secondNum;
+//
+//	cout << "Enter two different numbers: ";
+//
+//	cin >> firstNum >> secondNum;
+//
+//	firstNum > secondNum ?
+//		cout << "The FIRST number " << firstNum << " is GREATER than the SECOND number" << secondNum << ".\n" :
+//		cout << "The SECOND number " << secondNum << " is GREATER than the FIRST number" << firstNum << ".\n";
+//}
---
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad4.cpp
@@ -0,0 +1,43 @@
+﻿//4. Да се напише програма, която чете от конзолата 1 цяло число - проценти от изпит, и отпечатва оценката по следната скала :
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int percent;
+//	cout << "Enter your result: \n";
+//	cin >> percent;
+//
+//	if (percent >= 1 && percent <= 100)
+//	{
+//		if (percent >= 90)
+//		{
+//			cout << "Your grade is 6+!\n";
+//		}
+//		else if (percent >= 80 && percent < 90)
+//		{
+//			cout << "Your grade is 6!\n";
+//		}
+//		else if (percent >= 70 && percent < 80)
+//		{
+//			cout << "Your grade is 5!\n";
+//		}
+//		else if (percent >= 60 && percent < 70)
+//		{
+//			cout << "Your grade is 4!\n";
+//		}
+//		else if (percent >= 40 && percent < 60)
+//		{
+//			cout << "Your grade is 3!\n";
+//		}
+//		else
+//		{
+//			cout << "Your grade is 2!\n";
+//		}
+//	}
+//	else
+//	{
+//		cout << "The number you've entered is not a valid result!\n";
+//	}
+//}
