f475bc9d98f2de31900a94190cd7e7cd00407985
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-24 08:57:31 +0300

    prakt2 zad5

---
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad5.cpp
@@ -0,0 +1,26 @@
+﻿//5. Напишете програма, която приема от входа три числа - страни на триъгълник. <br / >
+//Програмата да проверява дали това е валиден триъгълник. <br / >
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() 
+//{
+//	double a, b, c;
+//
+//	cout << "Enter the lengths of the three sides of the triangle: ";
+//	cin >> a >> b >> c;
+//
+//	bool condition1 = (a + b > c), 
+//		condition2 = (a + c > b),
+//		condition3 = (b + c > a);
+//
+//	if (condition1 && condition2 && condition3)
+//	{
+//		cout << "A triangle with those sides IS valid.\n";
+//	}
+//	else
+//	{
+//		cout << "A triangle with those sides IS NOT valid.\n";
+//	}
+//}
