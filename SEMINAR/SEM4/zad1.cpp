bcc941f0d692d3c4e652b3c0d564713ecc6a64c1
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-28 17:16:52 +0200

    sem4 zad1 zad2

---
+++ SEMINAR/SEM4/zad1.cpp
@@ -0,0 +1,22 @@
+﻿// Задача 1:Напишете функция, която приема цяло число и връща модула му.
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int absoluteValue(int num)
+//{
+//	return (num < 0) ? -num : num;
+//}
+//
+//int main()
+//{
+//	int number;
+//
+//	cout << "Enter an integer: ";
+//	cin >> number;
+//
+//	cout << "The absolute value is: " << absoluteValue(number) << "\n";
+//
+//	return 0;
+//}
---
+++ SEMINAR/SEM4/zad2.cpp
@@ -0,0 +1,26 @@
+﻿//** Задача 2:**Напишете функция, която приема цяло число и връща броя на цифрите му.
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int countDigs(int num)
+//{
+//	int count = 0;
+//	
+//	do {
+//		count++;
+//	} while (num /= 10);
+//
+//	return count;
+//}
+//
+//int main()
+//{
+//	int number;
+//
+//	cout << "Enter an integer: ";
+//	cin >> number;
+//
+//	cout << "The number of digits is: " << countDigs(number) << "\n";
+//}
