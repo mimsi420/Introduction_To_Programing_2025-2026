6b7cee08763396bc2f506badc35dbd2012f704de
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-10-30 13:06:27 +0200

    sem2 zad3

---
+++ SEMINAR/SEM2/zad3.cpp
@@ -0,0 +1,38 @@
+﻿//** Задача 3 (Speed trap) : **Да се напише програма, която въвежда скорост(десетично число) и отпечатва
+//информация за скоростта.При скорост до 10 (включително)отпечатайте “slow”.При
+//скорост над 10 и до 50 отпечатайте “average”.При скорост над 50 и до 150 отпечатайте
+//“fast”.При скорост над 150 и до 1000 отпечатайте “ultra fast”.При по - висока скорост
+//отпечатайте “extremely fast”
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	double speed;
+//
+//	cout << "Enter your speed:";
+//	cin >> speed;
+//
+//	if (speed <= 10)
+//	{
+//		cout << "slow\n";
+//	}
+//	else if (speed > 10 && speed <= 50)
+//	{
+//		cout << "average\n";
+//	}
+//	else if (speed > 50 && speed <= 150)
+//	{
+//		cout << "fast\n";
+//	}
+//	else if (speed > 150 && speed <= 1000)
+//	{
+//		cout << "ultra fast\n";
+//	}
+//	else
+//	{
+//		cout << "extremely fast\n";
+//	}
+//}
