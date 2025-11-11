d76c487e5ab4b79f6dd2fbfd192337b26da7442c
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-11 14:46:14 +0200

    sem3 finished w/o 7

---
+++ SEMINAR/SEM3-done_bez_7/zad2.cpp
@@ -0,0 +1,27 @@
+﻿//** Задача 2:**Да се напише програма, която приема цяло положително число, обръща го и го увеличава с 1. < br / >
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int num, reversednum = 0;
+//
+//	cout << "enter a positive integer: \n";
+//	cin >> num;
+//
+//	if (num >= 0)
+//	{
+//		for (num; num != 0; num = num / 10)
+//		{
+//			reversednum = reversednum * 10 + num % 10;
+//		}
+//
+//		reversednum++;
+//		cout << "the reversed number 1 plus 1 is : " << reversednum << "\n";
+//	}
+//	else
+//	{
+//		cout << "the number is not positive or is not an integer.\n";
+//	}
+//}
---
+++ SEMINAR/SEM3-done_bez_7/zad4.cpp
@@ -0,0 +1,29 @@
+﻿//** Задача 4:**Напишете програма, която приема две естествени числа и връща най - големият им общ делител.
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int num1, num2, gcd = 1;
+//
+//	cout << "enter two natural numbers: \n";
+//	cin >> num1 >> num2; // 16 24
+//
+//	if (num1 == 0 || num2 == 0) 
+//	{
+//		cout << "Numbers must be non-zero.";
+//		return 0;
+//	}
+// 
+//	do
+//	{
+//		gcd = num1 % num2; // 16 % 24 = 16;  24 % 16 = 8;   16 % 8 = 0
+//		num1 = num2; // num1 = 24;   num1 = 16;   num1 = 8;
+//		num2 = gcd; // num2 = 16;   num2 = 8;	num2 = 0;
+//	} while (num2 != 0); // 16, 8, 0
+//
+//	gcd = num1; // gcd = 8
+//
+//	cout << "the greatest common divisor is : " << gcd << "\n";
+//}
