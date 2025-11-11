240a026c108009e28b0906558eaee6e9becb9210
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-11 08:57:13 +0200

    prakt2 zad 13

--- UP_PRAKTIKUM/PRAKTIKUM 2/zad12.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad12.cpp
@@ -1,40 +1,40 @@
 ﻿//12. Да се прочетат от конзолата 2 числа - ден и месец от годината.
 //Да се изведе следващия ден (ако се приеме, че годината не е високосна).
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int day, month, daysInMonth;
-	cout << "Enter day and month: ";
-	cin >> day >> month;
-
-	switch (month)
-	{
-		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-			daysInMonth = 31;
-			break;
-		case 4: case 6: case 9: case 11:
-			daysInMonth = 30;
-			break;
-		case 2:
-			daysInMonth = 28;
-			break;
-		default:
-			cout << "Invalid month!\n";
-			return 1;
-	}
-	
-	if (++day < daysInMonth)
-	{
-		cout << "Next day is: " << day << "." << month << "\n";
-	}
-	else
-	{
-		day = 1;
-		month == 12 ? month = 1 : month++;
-
-		cout << "Next day is: " << day << "." << month << "\n";
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int day, month, daysInMonth;
+//	cout << "Enter day and month: ";
+//	cin >> day >> month;
+//
+//	switch (month)
+//	{
+//		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+//			daysInMonth = 31;
+//			break;
+//		case 4: case 6: case 9: case 11:
+//			daysInMonth = 30;
+//			break;
+//		case 2:
+//			daysInMonth = 28;
+//			break;
+//		default:
+//			cout << "Invalid month!\n";
+//			return 1;
+//	}
+//	
+//	if (++day < daysInMonth)
+//	{
+//		cout << "Next day is: " << day << "." << month << "\n";
+//	}
+//	else
+//	{
+//		day = 1;
+//		month == 12 ? month = 1 : month++;
+//
+//		cout << "Next day is: " << day << "." << month << "\n";
+//	}
+//}
---
+++ UP_PRAKTIKUM/PRAKTIKUM 2/zad13.cpp
@@ -0,0 +1,36 @@
+﻿//13. Да се състави програма, чрез която се въвежда 4-цифренo естествено число от интервала[1000...9999]. 
+//От това число се формират 2 нови 2-цифрени числа.
+//Първото число се формира от 1-та и 4-та цифра на въведеното число.
+//Второто число се формира от 2-рa - 3-та цифра на въведеното число.
+// На екрана да се изведе дали 1 - то ново число e по-малко < , равно = или по-голямо от 2-то число.\
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number, first, second, dig1, dig2, dig3, dig4;
+	cout << "Enter a 4-digit natural number in the range [1000...9999]: ";
+	cin >> number;
+
+	dig1 = number / 1000; // 7689/1000 = 7
+	dig2 = number / 100 % 10; // 7689/100 = 76 % 10 = 6
+	dig3 = number / 10 % 10; // 7689/10 = 768 % 10 = 8
+	dig4 = number % 10; // 7689 % 10 = 9
+
+	first = dig1 * 10 + dig4; // 7*10 + 9 = 79
+	second = dig2 * 10 + dig3; // 6*10 + 8 = 68
+
+	if (first < second)
+	{
+		cout << "The FIRST number " << first << " is LESS than the SECOND number " << second << ".\n";
+	}
+	else if (first == second)
+	{
+		cout << "The FIRST number " << first << " is EQUAL to the SECOND number " << second << ".\n";
+	}
+	else
+	{
+		cout << "The FIRST number " << first << " is GREATER than the SECOND number " << second << ".\n";
+	}
+}
