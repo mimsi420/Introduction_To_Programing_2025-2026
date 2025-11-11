136cb193779ec315ebd9c5e42499389399550b5e
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-11 09:01:13 +0200

    prakt2 finished

--- UP_PRAKTIKUM/PRAKTIKUM 2-done/zad13.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 2-done/zad13.cpp
@@ -13,24 +13,32 @@ int main()
 	cout << "Enter a 4-digit natural number in the range [1000...9999]: ";
 	cin >> number;
 
-	dig1 = number / 1000; // 7689/1000 = 7
-	dig2 = number / 100 % 10; // 7689/100 = 76 % 10 = 6
-	dig3 = number / 10 % 10; // 7689/10 = 768 % 10 = 8
-	dig4 = number % 10; // 7689 % 10 = 9
-
-	first = dig1 * 10 + dig4; // 7*10 + 9 = 79
-	second = dig2 * 10 + dig3; // 6*10 + 8 = 68
-
-	if (first < second)
+	if (number > 1000 && number < 9999)
 	{
-		cout << "The FIRST number " << first << " is LESS than the SECOND number " << second << ".\n";
-	}
-	else if (first == second)
-	{
-		cout << "The FIRST number " << first << " is EQUAL to the SECOND number " << second << ".\n";
+		dig1 = number / 1000; // 7689/1000 = 7
+		dig2 = number / 100 % 10; // 7689/100 = 76 % 10 = 6
+		dig3 = number / 10 % 10; // 7689/10 = 768 % 10 = 8
+		dig4 = number % 10; // 7689 % 10 = 9
+
+		first = dig1 * 10 + dig4; // 7*10 + 9 = 79
+		second = dig2 * 10 + dig3; // 6*10 + 8 = 68
+
+		if (first < second)
+		{
+			cout << "The FIRST number " << first << " is LESS than the SECOND number " << second << ".\n";
+		}
+		else if (first == second)
+		{
+			cout << "The FIRST number " << first << " is EQUAL to the SECOND number " << second << ".\n";
+		}
+		else
+		{
+			cout << "The FIRST number " << first << " is GREATER than the SECOND number " << second << ".\n";
+		}
 	}
 	else
 	{
-		cout << "The FIRST number " << first << " is GREATER than the SECOND number " << second << ".\n";
+		cout << "The number is out of range!\n";
+		return 1;
 	}
 }
