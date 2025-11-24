de2a6e79cb02992aa48e778ef5aed9016befd315
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-24 19:26:15 +0200

    prakt3 zad3

--- UP_PRAKTIKUM/PRAKTIKUM 3/zad2.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad2.cpp
@@ -1,17 +1,17 @@
 ﻿//2. Да се състави програма, която по въведено естествено число n да изведе
 // всички естествени решения на уравнението x + y = n.
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int n;
-	cout << "Enter your number: ";
-	cin >> n;
-
-	for (int i = 1; i <= n; ++i)
-	{
-		cout << i << " + " << (n - i) << " = " << n << "\n";
-	}
-}
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cout << "Enter your number: ";
+//	cin >> n;
+//
+//	for (int i = 1; i <= n; ++i)
+//	{
+//		cout << i << " + " << (n - i) << " = " << n << "\n";
+//	}
+//}
---
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad3.cpp
@@ -0,0 +1,21 @@
+﻿//3. Напишете програма, която по въведено цяло положително число, извежда квадратът 
+// на това число, но в обратен ред, като водещите нули се игнорират.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, reversed = 0;
+	cout << "Enter a positive integer: ";
+	cin >> x;
+
+	int xSquared = x*x;
+
+	do {
+		reversed = reversed * 10 + (xSquared % 10);
+		xSquared /= 10;
+	} while (xSquared > 0);
+
+	cout << reversed;
+}
