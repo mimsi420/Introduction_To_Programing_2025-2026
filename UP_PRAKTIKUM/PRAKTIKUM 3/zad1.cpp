5e7fadca740646e99b1097baad184f3d89666873
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-24 11:44:06 +0200

    prakt3 zad1

---
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad1.cpp
@@ -0,0 +1,21 @@
+﻿//1. Да се напише програма, която приема естествено число n и изписва стълба от 1 до n
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Enter your number: ";
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= i; ++j)
+		{
+			cout << j;
+		}
+		cout << "\n";
+	}
+}
