b13ac9112333b696017c9a2ecff3925c533eeb35
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-25 12:15:58 +0200

    prakt3 zad7

--- UP_PRAKTIKUM/PRAKTIKUM 3/zad6.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad6.cpp
@@ -4,39 +4,38 @@
 //Tова продължава до въвеждане на число, равно на първоначалното.
 // Програмата да отпечата и броят направени предположения.
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int num, guess, guessCount = 0;
-	cout << "Enter an integer between 0 and 100: ";
-	cin >> num;
-	cout << "\n" << "\n" << "\n" << "\n" << "\n";
-
-	if (num >= 0 && num <= 100)
-	{
-		cout << "Try to guess the entered integer: ";
-		cin >> guess; guessCount++;
-
-		while (guess != num)
-		{
-			if (guess > num)
-			{
-				cout << "\nYour guessed number is GREATER than the entered one. Try again!\nEnter a number: ";
-			}
-			if (guess < num)
-			{
-				cout << "\nYour guessed number is LESS than the entered one. Try again!\nEnter a number: ";
-			}
-			cin >> guess; guessCount++;
-		}
-
-		cout << "\nYou guessed the number in " << guessCount <<" guesses!\n";
-	}
-	else
-	{
-		cout << "Enter a valid number (0-100)!";
-	}
-
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int num, guess, guessCount = 0;
+//	cout << "Enter an integer between 0 and 100: ";
+//	cin >> num;
+//	cout << "\n" << "\n" << "\n" << "\n" << "\n";
+//
+//	if (num >= 0 && num <= 100)
+//	{
+//		cout << "Try to guess the entered integer: ";
+//		cin >> guess; guessCount++;
+//
+//		while (guess != num)
+//		{
+//			if (guess > num)
+//			{
+//				cout << "\nYour guessed number is GREATER than the entered one. Try again!\nEnter a number: ";
+//			}
+//			if (guess < num)
+//			{
+//				cout << "\nYour guessed number is LESS than the entered one. Try again!\nEnter a number: ";
+//			}
+//			cin >> guess; guessCount++;
+//		}
+//
+//		cout << "\nYou guessed the number in " << guessCount <<" guesses!\n";
+//	}
+//	else
+//	{
+//		cout << "Enter a valid number (0-100)!";
+//	}
+//}
---
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad7.cpp
@@ -0,0 +1,27 @@
+﻿//7. Напишете програма, която иска от потребителя да въведе число N.
+// На следващите N - 1 реда, ще бъдат въведени всички
+//числа от 1 до N * *без едно * *.Програмата да изведе кое е пропуснатото число.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, entered, enteredSum = 0, missing = 0;
+	cout << "Enter an integer: ";
+	cin >> n;
+
+	int sum = (n * (n + 1) )/ 2;
+
+	cout << "Now enter " << n-1 << " numbers separated by enters: \n";
+
+	for (int i = 1; i < n; ++i)
+	{
+		cin >> entered;
+		enteredSum += entered;
+	}
+
+	missing = sum - enteredSum;
+
+	cout << "The missing number is: " << missing << "\n";
+}
