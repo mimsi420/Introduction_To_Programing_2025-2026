b17eb92b65fd37a7c0156039d095e281f4d30181
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-25 14:13:22 +0200

    prakt3 zad9

--- UP_PRAKTIKUM/PRAKTIKUM 3/zad8.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad8.cpp
@@ -2,29 +2,29 @@
 // отпечатва всички тройки естествени числа a, b и c,
 //не по - големи от N, които могат да бъдат страни на правоъгълен триъгълник.
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int n;
-	cout << "Enter a natural number: ";
-	cin >> n;
-
-	cout << "The natural three number combinations with numbers not greater than " << n 
-		<< ", that can form a triangle are:\n";
-
-	for (int a = 1; a <= n; ++a)
-	{
-		for (int b = 1; b <= n; ++b)
-		{
-			for (int c = 1; c <= n; ++c)
-			{
-				if (((a * a + b * b) == c * c) && (a <= b) && (b < c))
-				{
-					cout << "(" << a << " , " << b << " , " << c << ")\n";
-				}
-			}
-		}
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cout << "Enter a natural number: ";
+//	cin >> n;
+//
+//	cout << "The natural three number combinations with numbers not greater than " << n 
+//		<< ", that can form a triangle are:\n";
+//
+//	for (int a = 1; a <= n; ++a)
+//	{
+//		for (int b = 1; b <= n; ++b)
+//		{
+//			for (int c = 1; c <= n; ++c)
+//			{
+//				if (((a * a + b * b) == c * c) && (a <= b) && (b < c))
+//				{
+//					cout << "(" << a << " , " << b << " , " << c << ")\n";
+//				}
+//			}
+//		}
+//	}
+//}
