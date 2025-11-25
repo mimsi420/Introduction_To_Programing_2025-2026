8b729d03ea78b9367ef576d4f89708dc335d16af
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-25 16:01:03 +0200

    parkt3 zad11

--- UP_PRAKTIKUM/PRAKTIKUM 3/zad10.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad10.cpp
@@ -2,38 +2,38 @@
 // m и n и извежда всички перфектни числа в интервала[m, n].Перфектно е такова число N, 
 // което е равно на сумата от своите по - малки делители(т.е.различни от самото число).
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int m, n;
-	cout << "Enter two positive integers: ";
-	cin >> m >> n;
-
-	if (n > 0 && m > 0 && m < n)
-	{
-		cout << "The perfect numbers in the [" << m << ", " << n << "] interval are:\n";
-		for (int isPerfect = m; isPerfect <= n; ++isPerfect)
-		{
-			int sumDivs = 0;
-
-			for (int div = 1; div < isPerfect; ++div)
-			{
-				if (isPerfect % div == 0)
-				{
-					sumDivs += div;
-				}
-			}
-
-			if (isPerfect == sumDivs)
-			{
-				cout << isPerfect << " ";
-			}
-		}
-	}
-	else
-	{
-		cout << "Enter valid input! ";
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int m, n;
+//	cout << "Enter two positive integers: ";
+//	cin >> m >> n;
+//
+//	if (n > 0 && m > 0 && m < n)
+//	{
+//		cout << "The perfect numbers in the [" << m << ", " << n << "] interval are:\n";
+//		for (int isPerfect = m; isPerfect <= n; ++isPerfect)
+//		{
+//			int sumDivs = 0;
+//
+//			for (int div = 1; div < isPerfect; ++div)
+//			{
+//				if (isPerfect % div == 0)
+//				{
+//					sumDivs += div;
+//				}
+//			}
+//
+//			if (isPerfect == sumDivs)
+//			{
+//				cout << isPerfect << " ";
+//			}
+//		}
+//	}
+//	else
+//	{
+//		cout << "Enter valid input! ";
+//	}
+//}
