ab79bcb667a71501c7867cb6f6218d6a7e008b3c
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-25 15:30:33 +0200

    prakt3 zad10

---
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad10.cpp
@@ -0,0 +1,39 @@
+﻿//10. Да се напише програма, която чете от конзолата 2 цели положилени числа 
+// m и n и извежда всички перфектни числа в интервала[m, n].Перфектно е такова число N, 
+// което е равно на сумата от своите по - малки делители(т.е.различни от самото число).
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m, n;
+	cout << "Enter two positive integers: ";
+	cin >> m >> n;
+
+	if (n > 0 && m > 0 && m < n)
+	{
+		cout << "The perfect numbers in the [" << m << ", " << n << "] interval are:\n";
+		for (int isPerfect = m; isPerfect <= n; ++isPerfect)
+		{
+			int sumDivs = 0;
+
+			for (int div = 1; div < isPerfect; ++div)
+			{
+				if (isPerfect % div == 0)
+				{
+					sumDivs += div;
+				}
+			}
+
+			if (isPerfect == sumDivs)
+			{
+				cout << isPerfect << " ";
+			}
+		}
+	}
+	else
+	{
+		cout << "Enter valid input! ";
+	}
+}
--- UP_PRAKTIKUM/PRAKTIKUM 3/zad9.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad9.cpp
@@ -1,53 +1,53 @@
 ﻿//9. Напишете програма, която иска от потребителя вход цифра N[2; 9] и символ S.
 // Програмата да извежда фигура съставена от символа във вид на ромб, както е показано на примерите.
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int n;
-	char symb;
-
-	cout << "Enter a number between 2 and 9: ";
-	cin >> n;
-	cout << "Enter a symbol: ";
-	cin >> symb;
-
-	if (n >= 2 && n <= 9)
-	{
-		for (int row = 1; row <= n; ++row)
-		{
-			for (int spaces = 1; spaces <= n - row; ++spaces)
-			{
-				cout << ' ';
-			}
-			
-			for (int symbols = 1; symbols <= 2 * row - 1; ++symbols)
-			{
-				cout << symb;
-			}
-
-			cout << "\n";
-		}
-
-		for (int row = n - 1; row >= 1; --row)
-		{
-			for (int spaces = 1; spaces <= n - row; ++spaces)
-			{
-				cout << ' ';
-			}
-
-			for (int symbols = 1; symbols <= 2 * row - 1; ++symbols)
-			{
-				cout << symb;
-			}
-			
-			cout << "\n";
-		}
-	}
-	else
-	{
-		cout << "Enter a valid input!";
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	char symb;
+//
+//	cout << "Enter a number between 2 and 9: ";
+//	cin >> n;
+//	cout << "Enter a symbol: ";
+//	cin >> symb;
+//
+//	if (n >= 2 && n <= 9)
+//	{
+//		for (int row = 1; row <= n; ++row)
+//		{
+//			for (int spaces = 1; spaces <= n - row; ++spaces)
+//			{
+//				cout << ' ';
+//			}
+//			
+//			for (int symbols = 1; symbols <= 2 * row - 1; ++symbols)
+//			{
+//				cout << symb;
+//			}
+//
+//			cout << "\n";
+//		}
+//
+//		for (int row = n - 1; row >= 1; --row)
+//		{
+//			for (int spaces = 1; spaces <= n - row; ++spaces)
+//			{
+//				cout << ' ';
+//			}
+//
+//			for (int symbols = 1; symbols <= 2 * row - 1; ++symbols)
+//			{
+//				cout << symb;
+//			}
+//			
+//			cout << "\n";
+//		}
+//	}
+//	else
+//	{
+//		cout << "Enter a valid input!";
+//	}
+//}
