22084b1d335575bb9f0bbce5ea6b174eebee47bb
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-25 20:00:14 +0200

    prakt3 zad14

--- UP_PRAKTIKUM/PRAKTIKUM 3/zad13.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad13.cpp
@@ -1,35 +1,35 @@
 ﻿//13. Да се напише програма, която приема 2 числa - a и b и отпечатва всички прости числа в интервала[a, b].
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int a, b;
-	cout << "enter two integers: ";
-	cin >> a >> b;
-	
-	cout << "The prime numbers between " << a << " and " << b << " are:\n";
-
-	for (int num = a; num <= b; ++num)
-	{ 
-		if (num < 2)
-			continue;
-
-		bool isPrime = true;
-
-		for (int div = 2; div < num; ++div) // 2 3 4 5 6 
-		{
-			if (num % div == 0) // 7 % 2 = 1; 7 % 3 = 1 ...
-			{
-				isPrime = false;
-				break;
-			}
-		}
-
-		if (isPrime)
-		{
-			cout << num << " ";
-		}
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b;
+//	cout << "enter two integers: ";
+//	cin >> a >> b;
+//	
+//	cout << "The prime numbers between " << a << " and " << b << " are:\n";
+//
+//	for (int num = a; num <= b; ++num)
+//	{ 
+//		if (num < 2)
+//			continue;
+//
+//		bool isPrime = true;
+//
+//		for (int div = 2; div < num; ++div) // 2 3 4 5 6 
+//		{
+//			if (num % div == 0) // 7 % 2 = 1; 7 % 3 = 1 ...
+//			{
+//				isPrime = false;
+//				break;
+//			}
+//		}
+//
+//		if (isPrime)
+//		{
+//			cout << num << " ";
+//		}
+//	}
+//}
---
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad14.cpp
@@ -0,0 +1,24 @@
+﻿//14. Да се напише програма, която приема цяло число и отпечатва дали е степен на 2 - ката.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Not a power of 2!\n";
+        return 0;
+    }
+
+    while (n % 2 == 0) {
+        n /= 2;
+    }
+
+    if (n == 1)
+        cout << "a power of 2\n";
+    else
+        cout << "Not a power of 2!\n";
+}
