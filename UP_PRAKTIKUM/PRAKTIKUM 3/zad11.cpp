8530342b925175807f4d674328cb80ac352eb511
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-25 17:14:01 +0200

    prakt3 zad13

--- UP_PRAKTIKUM/PRAKTIKUM 3/zad11.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad11.cpp
@@ -1,31 +1,31 @@
 ﻿//Напишете програма, която при въведено число n > 3 извежда редица от числата на Фибоначи ненадвишаващи n.
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int n, nextNum = 0, secondToLast = 2, last = 3;
-	cout << "Enter an integer bigger than 3: ";
-	cin >> n;
-
-	if (n > 3)
-	{
-		cout << "1 1 2 3";
-		while (true)
-		{
-			nextNum = secondToLast + last;
-
-			if (nextNum > n) break;
-
-			cout << " " << nextNum;
-
-			secondToLast = last;
-			last = nextNum;
-		}
-	}
-	else
-	{
-		cout << "Enter valid inpit!";
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, nextNum = 0, secondToLast = 2, last = 3;
+//	cout << "Enter an integer bigger than 3: ";
+//	cin >> n;
+//
+//	if (n > 3)
+//	{
+//		cout << "1 1 2 3";
+//		while (true)
+//		{
+//			nextNum = secondToLast + last;
+//
+//			if (nextNum > n) break;
+//
+//			cout << " " << nextNum;
+//
+//			secondToLast = last;
+//			last = nextNum;
+//		}
+//	}
+//	else
+//	{
+//		cout << "Enter valid inpit!";
+//	}
+//}
