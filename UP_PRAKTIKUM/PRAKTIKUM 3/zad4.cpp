8abb5235806082774ca6dc102af81f51c54828ad
Maria Malcheva <87224276+mimsi420@users.noreply.github.com>
2025-11-25 11:31:15 +0200

    prakt3 zad5

--- UP_PRAKTIKUM/PRAKTIKUM 3/zad4.cpp
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad4.cpp
@@ -1,18 +1,18 @@
 ﻿//4. Да се прочетe от конзолата цяло число и да се намери факториелът му.
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int n, factorial = 1;
-	cout << "Enter an integer: ";
-	cin >> n;
-
-	for (n; n > 1; --n)
-	{
-		factorial *= n;
-	}
-	
-	cout << factorial;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, factorial = 1;
+//	cout << "Enter an integer: ";
+//	cin >> n;
+//
+//	for (n; n > 1; --n)
+//	{
+//		factorial *= n;
+//	}
+//	
+//	cout << factorial;
+//}
---
+++ UP_PRAKTIKUM/PRAKTIKUM 3/zad5.cpp
@@ -0,0 +1,28 @@
+﻿//5. Напишете програма, която приема от потребителя две цели положителни числа - *m * и * n * .
+//Програмата да отпечата числата от 1 до m * n, като на всеки ред има по* n* числа(тоест ще отпечатате* m* реда).
+//Печатайте числата по редове :
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m, n;
+	cout << "Enter two positive integers: \n";
+	cin >> m >> n;
+	cout << "\n";
+
+	int result = m * n;
+	
+	for (int i = 1; i <= result; ++i)
+	{
+		if (i % n == 0)
+		{
+			cout << i << "\n";
+		}
+		else
+		{
+			cout << i;
+		}
+	}
+}
